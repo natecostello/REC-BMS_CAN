5b98346f5e60c71e78af1dfd15253dbd1c87ce28
Nate Costello <natecostello@gmail.com>
2021-04-10 11:44:19 -0400

    updated timing structure

--- REC-BMS-CAN-logger.py
+++ REC-BMS-CAN-logger.py
@@ -96,49 +96,62 @@ headers = '| {0:27s} | {1:3s} '.format('Time Stamp', 'cnt')
 headers += '| {0:4s} | {1:5s} | {2:5s} | {3:4s} | {4:3s} | {5:3s} | {6:6s} | {7:5s} | {8:5s} | {9:4s} | {10:5s} | {11:5s} | {12:3s} | {13:3s} |'.format('CVL', 'CCL', 'DCL', 'DVL', 'SOC', 'SOH', 'SOC', 'BaV', 'BaC', 'BaT', 'MiV', 'MaV', 'MiT', 'MaT')
 print('\r {} '.format(headers))
 # Main loop
+
+cdl_updated = False
+soc_soh_updated = False
+bvct_updated = False
+min_max_cell_updated = False
+
 try:
 	while True:
-		for i in range(14):
-			while(q.empty() == True):	# Wait until there is a message
-				pass
-			message = q.get()
-			ts = datetime.datetime.fromtimestamp(message.timestamp).strftime('%Y-%m-%d %H:%M:%S.%f')
+		while(q.empty() == True):	# Wait until there is a message
+			pass
+		message = q.get()
+		ts = datetime.datetime.fromtimestamp(message.timestamp).strftime('%Y-%m-%d %H:%M:%S.%f')
 
-			c = '| {0:27s} | {1:3d} '.format(ts,count)
+		c = '| {0:27s} | {1:3d} '.format(ts,count)
 
 
-			if message.arbitration_id == CHARGE_DISCHARGE_LIMITS_ID:
-				charge_voltage_limit = 0.1 * int.from_bytes(message.data[0:2], 'little')
-				charge_current_limit = 0.1 * int.from_bytes(message.data[2:4], 'little')
-				discharge_current_limit = 0.1 * int.from_bytes(message.data[4:6], 'little')
-				discharge_voltage_limit = 0.1 * int.from_bytes(message.data[6:8], 'little')
+		if message.arbitration_id == CHARGE_DISCHARGE_LIMITS_ID:
+			charge_voltage_limit = 0.1 * int.from_bytes(message.data[0:2], 'little')
+			charge_current_limit = 0.1 * int.from_bytes(message.data[2:4], 'little')
+			discharge_current_limit = 0.1 * int.from_bytes(message.data[4:6], 'little')
+			discharge_voltage_limit = 0.1 * int.from_bytes(message.data[6:8], 'little')
+			cdl_updated = True
 
-			if message.arbitration_id == SOC_SOH_ID:
-				state_of_charge = int.from_bytes(message.data[0:2], 'little')
-				state_of_health = int.from_bytes(message.data[2:4], 'little')
-				state_of_charge_hi_res = 0.01 * int.from_bytes(message.data[4:6], 'little')
-					
-			if message.arbitration_id == BATTERY_VOLT_CURRENT_TEMP_ID:
-				battery_voltage = 0.01 * int.from_bytes(message.data[0:2], 'little')
-				battery_current = 0.1 * int.from_bytes(message.data[2:4], 'little')
-				battery_temperature = 0.1 * int.from_bytes(message.data[4:6], 'little')
+		if message.arbitration_id == SOC_SOH_ID:
+			state_of_charge = int.from_bytes(message.data[0:2], 'little')
+			state_of_health = int.from_bytes(message.data[2:4], 'little')
+			state_of_charge_hi_res = 0.01 * int.from_bytes(message.data[4:6], 'little')
+			soc_soh_updated = True
+				
+		if message.arbitration_id == BATTERY_VOLT_CURRENT_TEMP_ID:
+			battery_voltage = 0.01 * int.from_bytes(message.data[0:2], 'little')
+			battery_current = 0.1 * int.from_bytes(message.data[2:4], 'little')
+			battery_temperature = 0.1 * int.from_bytes(message.data[4:6], 'little')
+			bvct_updated = True
 
-			if message.arbitration_id == MIN_MAX_CELL_VOLT_TEMP_ID:
-				min_cell_voltage = 0.001 * int.from_bytes(message.data[0:2], 'little')
-				max_cell_voltage = 0.001 * int.from_bytes(message.data[2:4], 'little')
-				min_temperature = int.from_bytes(message.data[4:6], 'little')
-				max_temperature = int.from_bytes(message.data[6:8], 'little')
+		if message.arbitration_id == MIN_MAX_CELL_VOLT_TEMP_ID:
+			min_cell_voltage = 0.001 * int.from_bytes(message.data[0:2], 'little')
+			max_cell_voltage = 0.001 * int.from_bytes(message.data[2:4], 'little')
+			min_temperature = int.from_bytes(message.data[4:6], 'little')
+			max_temperature = int.from_bytes(message.data[6:8], 'little')
+			min_max_cell_updated = True
 
 		#c += '{0:.1f},{1:.1f},{2:.1f},{3:.1f},{4:.0f},{5:.0f},{6:.2f},{7:.2f},{8:.1f},{9:.1f},{10:.3f},{11:.3f},{12:.0f},{13:.0f}'.format(charge_voltage_limit, charge_current_limit, discharge_current_limit, discharge_voltage_limit, state_of_charge, state_of_health, state_of_charge_hi_res, battery_voltage, battery_current, battery_temperature, min_cell_voltage, max_cell_voltage, min_temperature, max_temperature)
-		c += '| {0: >4.1f} | {1: >5.1f} | {2: >5.1f} | {3: >4.1f} | {4: >3d} | {5:0>3d} | {6: >6.2f} | {7: >5.2f} | {8: >5.1f} | {9: >4.1f} | {10: >5.3f} | {11: >5.3f} | {12: >3.0f} | {13: >3.0f} |'.format(charge_voltage_limit, charge_current_limit, discharge_current_limit, discharge_voltage_limit, state_of_charge, state_of_health, state_of_charge_hi_res, battery_voltage, battery_current, battery_temperature, min_cell_voltage, max_cell_voltage, min_temperature, max_temperature)
-		print('\r {} '.format(c))
-		print('\r {} '.format(headers), end="\r")
-			
-		#print(c, file=outfile) # Save data to file
-		count += 1
-		# if count % 10 == 0:
-		# 	print('\r {} '.format(headers))
-			
+		if cdl_updated and soc_soh_updated and bvct_updated and min_max_cell_updated:
+			c += '| {0: >4.1f} | {1: >5.1f} | {2: >5.1f} | {3: >4.1f} | {4: >3d} | {5:0>3d} | {6: >6.2f} | {7: >5.2f} | {8: >5.1f} | {9: >4.1f} | {10: >5.3f} | {11: >5.3f} | {12: >3.0f} | {13: >3.0f} |'.format(charge_voltage_limit, charge_current_limit, discharge_current_limit, discharge_voltage_limit, state_of_charge, state_of_health, state_of_charge_hi_res, battery_voltage, battery_current, battery_temperature, min_cell_voltage, max_cell_voltage, min_temperature, max_temperature)
+			print('\r {} '.format(c))
+			print('\r {} '.format(headers), end="\r")
+			#print(c, file=outfile) # Save data to file
+			count += 1
+			# if count % 10 == 0:
+			# 	print('\r {} '.format(headers))
+			cdl_updated = False
+			soc_soh_updated = False
+			bvct_updated = False
+			min_max_cell_updated = False
+
 
  
 	
